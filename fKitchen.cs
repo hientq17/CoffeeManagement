2f7d429ce5c36fc787db838ace6d78a3088f73bf
Trinh Quang Hien <quanghien1712@gmail.com>
2020-11-19 21:43:39 +0700

    fix fKitchen

--- fKitchen.cs
+++ fKitchen.cs
@@ -11,35 +11,28 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace CoffeeManagement
-{
-    public partial class fKitchen : Form
-    {
+namespace CoffeeManagement {
+    public partial class fKitchen : Form {
         private string username;
-       
-        private void flpTable_Paint(object sender, PaintEventArgs e)
-        {
+
+        private void flpTable_Paint(object sender, PaintEventArgs e) {
 
         }
-        public fKitchen(string username)
-        {
+        public fKitchen(string username) {
             InitializeComponent();
             LoadTableByCategory("Đang đợi");
             this.username = username;
         }
 
         #region method
-      
 
-        private void LoadTableByCategory(string category)
-        {           
-         
+
+        private void LoadTableByCategory(string category) {
+
             flpTable.Controls.Clear();
             List<CoffeeTable> tableList = CoffeeTableDAO.Instance.LoadTableList();
-            foreach (CoffeeTable item in tableList)
-            {
-                if (item.Status.Equals(category))
-                {
+            foreach (CoffeeTable item in tableList) {
+                if (item.Status.Equals(category)) {
                     Button btn = new Button() { Width = CoffeeTableDAO.TableWidth, Height = CoffeeTableDAO.TableHeight };
                     btn.Text = item.ID + Environment.NewLine + item.Status;
                     btn.Click += btnTable_Click;
@@ -51,14 +44,12 @@ namespace CoffeeManagement
 
         }
 
-        private void ShowBill(int id)
-        {
+        private void ShowBill(int id) {
             lbTable.Text = "Bàn " + id;
             int invoiceID = InvoiceDAO.Instance.GetUncheckoutInvoiceIDByTableID(id);
             listViewInvoice.Items.Clear();
             List<InvoiceDetail> listInvoiceDetail = InvoiceDetailDAO.Instance.GetListInvoiceDetailByTable(invoiceID);
-            foreach (InvoiceDetail item in listInvoiceDetail)
-            {
+            foreach (InvoiceDetail item in listInvoiceDetail) {
                 ListViewItem lsvItem = new ListViewItem(item.ProductName.ToString());
                 lsvItem.SubItems.Add(item.Count.ToString());
                 lsvItem.SubItems.Add(item.Price.ToString());
@@ -66,12 +57,11 @@ namespace CoffeeManagement
                 lsvItem.SubItems.Add(item.ProductId.ToString());
                 listViewInvoice.Items.Add(lsvItem);
             }
-          
+
         }
 
 
-        private void btnTable_Click(object sender, EventArgs e)
-        {
+        private void btnTable_Click(object sender, EventArgs e) {
 
             int tableID = ((sender as Button).Tag as CoffeeTable).ID;
             string status = ((sender as Button).Tag as CoffeeTable).Status;
@@ -79,56 +69,50 @@ namespace CoffeeManagement
 
         }
 
-        
 
-        private void btnCheckOut_Click(object sender, EventArgs e)
-        {
+
+        private void btnCheckOut_Click(object sender, EventArgs e) {
 
             int tableId = int.Parse(lbTable.Text.Substring(3));
             string date = DateTime.Now.ToString("yyyy-MM-dd");
             ConnectDB.Instance.ExecuteNonQuery("update CoffeeTable set tableStatus = 1 where tableId = '" + tableId + "'");
             ConnectDB.Instance.ExecuteNonQuery("Insert into Invoice (tableId,dateSale,employeeUser, invoiceStatus) values (" + tableId + ", '" + date + "','" + this.Text.Substring(6) + "'," + 1 + ")");
             int invoiceId = InvoiceDAO.Instance.GetUncheckoutInvoiceIDByTableID(tableId);
-           /* insertInvoiceDetail(invoiceId);*/
-           /* LoadTable();*/
+            /* insertInvoiceDetail(invoiceId);*/
+            /* LoadTable();*/
             listViewInvoice.Items.Clear();
             lbTable.Text = "";
-           
+
         }
 
-        private void listViewInvoice_SelectedIndexChanged(object sender, EventArgs e)
-        {
-           
+        private void listViewInvoice_SelectedIndexChanged(object sender, EventArgs e) {
+
         }
-        #endregion       
+        #endregion
 
-     
 
-        private void cbxCategory_SelectedIndexChanged(object sender, EventArgs e)
-        {
-          
+
+        private void cbxCategory_SelectedIndexChanged(object sender, EventArgs e) {
+
         }
 
-        private void đăngXuấtToolStripMenuItem_Click(object sender, EventArgs e)
-        {
+        private void đăngXuấtToolStripMenuItem_Click(object sender, EventArgs e) {
             DialogResult dr = MessageBox.Show("Bạn muốn đăng xuất ?", "Title", MessageBoxButtons.YesNo,
        MessageBoxIcon.Information);
             if (dr == DialogResult.Yes)
                 Environment.Exit(0);
         }
 
-        private void btnHoanThanh_Click(object sender, EventArgs e)
-        {
-            int tableID =Convert.ToInt32( lbTable.Text.Substring(4));
-           
+        private void btnHoanThanh_Click(object sender, EventArgs e) {
+            int tableID = Convert.ToInt32(lbTable.Text.Substring(4));
+
             int invoiceId = InvoiceDAO.Instance.GetUncheckoutInvoiceIDByTableID(tableID);
 
-            if(CoffeeTableDAO.Instance.UpdateTable(tableID)!=false &&
-            InvoiceDAO.Instance.UpdateInvoice(invoiceId)!= false)
-            {
+            if (CoffeeTableDAO.Instance.UpdateTable(tableID) != false &&
+            InvoiceDAO.Instance.UpdateInvoice(invoiceId) != false) {
                 MessageBox.Show("Hoan thanh bill");
                 LoadTableByCategory("Đang đợi");
             }
         }
     }
-    }
+}
