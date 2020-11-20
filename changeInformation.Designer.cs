c75709dab78957c40b779374c01dc9df02bc7ccb
Trinh Quang Hien <quanghien1712@gmail.com>
2020-11-20 20:41:06 +0700

    Update change information fadmin

--- changeInformation.Designer.cs
+++ changeInformation.Designer.cs
@@ -23,10 +23,6 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.panel13 = new System.Windows.Forms.Panel();
-            this.panel20 = new System.Windows.Forms.Panel();
-            this.cbbRoleId = new System.Windows.Forms.ComboBox();
-            this.label16 = new System.Windows.Forms.Label();
             this.panel17 = new System.Windows.Forms.Panel();
             this.label15 = new System.Windows.Forms.Label();
             this.txtEmployeeName = new System.Windows.Forms.TextBox();
@@ -36,61 +32,33 @@
             this.panelEmployeeOK = new System.Windows.Forms.Panel();
             this.btnEmployeeCancel = new System.Windows.Forms.Button();
             this.btnEmployeeOK = new System.Windows.Forms.Button();
-            this.panel13.SuspendLayout();
-            this.panel20.SuspendLayout();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.txtRepeatPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.txtNewPassword = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txtOldPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnCheck = new System.Windows.Forms.Button();
             this.panel17.SuspendLayout();
             this.panel18.SuspendLayout();
             this.panelEmployeeOK.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // panel13
-            // 
-            this.panel13.Controls.Add(this.panel20);
-            this.panel13.Controls.Add(this.panel17);
-            this.panel13.Controls.Add(this.panel18);
-            this.panel13.Controls.Add(this.panelEmployeeOK);
-            this.panel13.Location = new System.Drawing.Point(73, 15);
-            this.panel13.Name = "panel13";
-            this.panel13.Size = new System.Drawing.Size(304, 209);
-            this.panel13.TabIndex = 3;
-            // 
-            // panel20
-            // 
-            this.panel20.Controls.Add(this.cbbRoleId);
-            this.panel20.Controls.Add(this.label16);
-            this.panel20.Location = new System.Drawing.Point(14, 93);
-            this.panel20.Margin = new System.Windows.Forms.Padding(2);
-            this.panel20.Name = "panel20";
-            this.panel20.Size = new System.Drawing.Size(280, 36);
-            this.panel20.TabIndex = 1;
-            // 
-            // cbbRoleId
-            // 
-            this.cbbRoleId.Enabled = false;
-            this.cbbRoleId.FormattingEnabled = true;
-            this.cbbRoleId.Items.AddRange(new object[] {
-            "Thu ngân",
-            "Pha chế"});
-            this.cbbRoleId.Location = new System.Drawing.Point(89, 7);
-            this.cbbRoleId.Name = "cbbRoleId";
-            this.cbbRoleId.Size = new System.Drawing.Size(181, 21);
-            this.cbbRoleId.TabIndex = 2;
-            // 
-            // label16
-            // 
-            this.label16.AutoSize = true;
-            this.label16.Location = new System.Drawing.Point(19, 10);
-            this.label16.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label16.Name = "label16";
-            this.label16.Size = new System.Drawing.Size(50, 13);
-            this.label16.TabIndex = 1;
-            this.label16.Text = "Chức vụ:";
             // 
             // panel17
             // 
             this.panel17.Controls.Add(this.label15);
             this.panel17.Controls.Add(this.txtEmployeeName);
-            this.panel17.Location = new System.Drawing.Point(14, 53);
+            this.panel17.Location = new System.Drawing.Point(14, 67);
             this.panel17.Margin = new System.Windows.Forms.Padding(2);
             this.panel17.Name = "panel17";
             this.panel17.Size = new System.Drawing.Size(280, 36);
@@ -99,16 +67,15 @@
             // label15
             // 
             this.label15.AutoSize = true;
-            this.label15.Location = new System.Drawing.Point(4, 10);
+            this.label15.Location = new System.Drawing.Point(19, 10);
             this.label15.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label15.Name = "label15";
-            this.label15.Size = new System.Drawing.Size(79, 13);
+            this.label15.Size = new System.Drawing.Size(57, 13);
             this.label15.TabIndex = 1;
-            this.label15.Text = "Tên nhân viên:";
+            this.label15.Text = "Họ và tên:";
             // 
             // txtEmployeeName
             // 
-            this.txtEmployeeName.Enabled = false;
             this.txtEmployeeName.Location = new System.Drawing.Point(87, 7);
             this.txtEmployeeName.Margin = new System.Windows.Forms.Padding(2);
             this.txtEmployeeName.Name = "txtEmployeeName";
@@ -119,7 +86,7 @@
             // 
             this.panel18.Controls.Add(this.label1212);
             this.panel18.Controls.Add(this.txtEmployeeUser);
-            this.panel18.Location = new System.Drawing.Point(14, 13);
+            this.panel18.Location = new System.Drawing.Point(14, 18);
             this.panel18.Margin = new System.Windows.Forms.Padding(2);
             this.panel18.Name = "panel18";
             this.panel18.Size = new System.Drawing.Size(280, 36);
@@ -148,59 +115,186 @@
             // 
             this.panelEmployeeOK.Controls.Add(this.btnEmployeeCancel);
             this.panelEmployeeOK.Controls.Add(this.btnEmployeeOK);
-            this.panelEmployeeOK.Location = new System.Drawing.Point(14, 133);
+            this.panelEmployeeOK.Location = new System.Drawing.Point(85, 328);
             this.panelEmployeeOK.Margin = new System.Windows.Forms.Padding(2);
             this.panelEmployeeOK.Name = "panelEmployeeOK";
-            this.panelEmployeeOK.Size = new System.Drawing.Size(280, 58);
+            this.panelEmployeeOK.Size = new System.Drawing.Size(360, 58);
             this.panelEmployeeOK.TabIndex = 4;
-            this.panelEmployeeOK.Visible = false;
             // 
             // btnEmployeeCancel
             // 
-            this.btnEmployeeCancel.Location = new System.Drawing.Point(153, 15);
+            this.btnEmployeeCancel.Location = new System.Drawing.Point(177, 15);
             this.btnEmployeeCancel.Margin = new System.Windows.Forms.Padding(2);
             this.btnEmployeeCancel.Name = "btnEmployeeCancel";
             this.btnEmployeeCancel.Size = new System.Drawing.Size(56, 29);
             this.btnEmployeeCancel.TabIndex = 0;
             this.btnEmployeeCancel.Text = "Hủy";
             this.btnEmployeeCancel.UseVisualStyleBackColor = true;
+            this.btnEmployeeCancel.Click += new System.EventHandler(this.btnEmployeeCancel_Click);
             // 
             // btnEmployeeOK
             // 
-            this.btnEmployeeOK.Location = new System.Drawing.Point(78, 15);
+            this.btnEmployeeOK.Location = new System.Drawing.Point(107, 15);
             this.btnEmployeeOK.Margin = new System.Windows.Forms.Padding(2);
             this.btnEmployeeOK.Name = "btnEmployeeOK";
             this.btnEmployeeOK.Size = new System.Drawing.Size(56, 29);
             this.btnEmployeeOK.TabIndex = 0;
             this.btnEmployeeOK.Text = "OK";
             this.btnEmployeeOK.UseVisualStyleBackColor = true;
+            this.btnEmployeeOK.Click += new System.EventHandler(this.btnEmployeeOK_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnCheck);
+            this.groupBox1.Controls.Add(this.panel2);
+            this.groupBox1.Controls.Add(this.panel4);
+            this.groupBox1.Controls.Add(this.panel3);
+            this.groupBox1.Location = new System.Drawing.Point(71, 152);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(374, 171);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Đổi mật khẩu";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.txtRepeatPassword);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(14, 118);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(280, 36);
+            this.panel2.TabIndex = 2;
+            // 
+            // txtRepeatPassword
+            // 
+            this.txtRepeatPassword.Enabled = false;
+            this.txtRepeatPassword.Location = new System.Drawing.Point(107, 7);
+            this.txtRepeatPassword.Margin = new System.Windows.Forms.Padding(2);
+            this.txtRepeatPassword.Name = "txtRepeatPassword";
+            this.txtRepeatPassword.PasswordChar = '*';
+            this.txtRepeatPassword.Size = new System.Drawing.Size(161, 20);
+            this.txtRepeatPassword.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 10);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(99, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nhập lại mật khẩu: ";
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.txtNewPassword);
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Location = new System.Drawing.Point(14, 68);
+            this.panel4.Margin = new System.Windows.Forms.Padding(2);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(280, 36);
+            this.panel4.TabIndex = 3;
+            // 
+            // txtNewPassword
+            // 
+            this.txtNewPassword.Enabled = false;
+            this.txtNewPassword.Location = new System.Drawing.Point(107, 7);
+            this.txtNewPassword.Margin = new System.Windows.Forms.Padding(2);
+            this.txtNewPassword.Name = "txtNewPassword";
+            this.txtNewPassword.PasswordChar = '*';
+            this.txtNewPassword.Size = new System.Drawing.Size(161, 20);
+            this.txtNewPassword.TabIndex = 2;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 10);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Mật khẩu mới: ";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.txtOldPassword);
+            this.panel3.Controls.Add(this.label3);
+            this.panel3.Location = new System.Drawing.Point(14, 18);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(280, 36);
+            this.panel3.TabIndex = 3;
+            // 
+            // txtOldPassword
+            // 
+            this.txtOldPassword.Location = new System.Drawing.Point(107, 7);
+            this.txtOldPassword.Margin = new System.Windows.Forms.Padding(2);
+            this.txtOldPassword.Name = "txtOldPassword";
+            this.txtOldPassword.PasswordChar = '*';
+            this.txtOldPassword.Size = new System.Drawing.Size(161, 20);
+            this.txtOldPassword.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 10);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Mật khẩu cũ: ";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.panel17);
+            this.groupBox2.Controls.Add(this.panel18);
+            this.groupBox2.Location = new System.Drawing.Point(71, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(374, 118);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Đổi thông tin";
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(298, 20);
+            this.btnCheck.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(60, 29);
+            this.btnCheck.TabIndex = 1;
+            this.btnCheck.Text = "Kiểm tra";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
             // 
             // changeInformation
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(444, 231);
-            this.Controls.Add(this.panel13);
+            this.ClientSize = new System.Drawing.Size(507, 406);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panelEmployeeOK);
             this.Name = "changeInformation";
             this.Text = "Chỉnh sửa thông tin";
-            this.panel13.ResumeLayout(false);
-            this.panel20.ResumeLayout(false);
-            this.panel20.PerformLayout();
             this.panel17.ResumeLayout(false);
             this.panel17.PerformLayout();
             this.panel18.ResumeLayout(false);
             this.panel18.PerformLayout();
             this.panelEmployeeOK.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel13;
-        private System.Windows.Forms.Panel panel20;
-        private System.Windows.Forms.ComboBox cbbRoleId;
-        private System.Windows.Forms.Label label16;
         private System.Windows.Forms.Panel panel17;
         private System.Windows.Forms.Label label15;
         private System.Windows.Forms.TextBox txtEmployeeName;
@@ -210,5 +304,17 @@
         private System.Windows.Forms.Panel panelEmployeeOK;
         private System.Windows.Forms.Button btnEmployeeCancel;
         private System.Windows.Forms.Button btnEmployeeOK;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox txtRepeatPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.TextBox txtNewPassword;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox txtOldPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnCheck;
     }
 }
